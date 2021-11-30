e133c5bdd6377c6fc15159976dc83e3749d89a53
nicolas cros <nicolas.cros@luos.io>
2021-11-30 13:34:43 +0100

    compute pixel color depending on triangle position

--- webgpu/src/main.rs
+++ webgpu/src/main.rs
@@ -12,6 +12,8 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
+    render_pipeline_color: wgpu::RenderPipeline,
+    choose_pipeline: bool,
 }
 
 impl State {
@@ -102,6 +104,46 @@ impl State {
             },
         });
 
+        let render_pipeline_color =
+            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
+                label: Some("Render Pipeline"),
+                layout: Some(&render_pipeline_layout),
+                vertex: wgpu::VertexState {
+                    module: &shader,
+                    entry_point: "vs_main_color",
+                    buffers: &[],
+                },
+                fragment: Some(wgpu::FragmentState {
+                    module: &shader,
+                    entry_point: "fs_main_color",
+                    targets: &[wgpu::ColorTargetState {
+                        format: config.format,
+                        blend: Some(wgpu::BlendState::REPLACE),
+                        write_mask: wgpu::ColorWrites::ALL,
+                    }],
+                }),
+                primitive: wgpu::PrimitiveState {
+                    topology: wgpu::PrimitiveTopology::TriangleList,
+                    strip_index_format: None,
+                    front_face: wgpu::FrontFace::Ccw,
+                    cull_mode: Some(wgpu::Face::Back),
+                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
+                    polygon_mode: wgpu::PolygonMode::Fill,
+                    // Requires Features::DEPTH_CLAMPING
+                    clamp_depth: false,
+                    // Requires Features::CONSERVATIVE_RASTERIZATION
+                    conservative: false,
+                },
+                depth_stencil: None,
+                multisample: wgpu::MultisampleState {
+                    count: 1,
+                    mask: !0,
+                    alpha_to_coverage_enabled: false,
+                },
+            });
+
+        let choose_pipeline = true;
+
         Self {
             surface,
             device,
@@ -109,6 +151,8 @@ impl State {
             config,
             size,
             render_pipeline,
+            render_pipeline_color,
+            choose_pipeline,
         }
     }
 
@@ -136,6 +180,7 @@ impl State {
                 color.g = 0.8;
                 color.b = 0.1;
                 color.a = 1.0;
+                true
             }
             WindowEvent::KeyboardInput {
                 input:
@@ -147,13 +192,26 @@ impl State {
                 ..
             } => {
                 color.r = 0.1;
-                color.g = 0.1;
-                color.b = 0.8;
+                color.g = 0.2;
+                color.b = 0.3;
                 color.a = 1.0;
+                true
             }
-            _ => {}
+            WindowEvent::KeyboardInput {
+                input:
+                    KeyboardInput {
+                        state: ElementState::Pressed,
+                        virtual_keycode: Some(VirtualKeyCode::Space),
+                        ..
+                    },
+                ..
+            } => {
+                self.choose_pipeline = !self.choose_pipeline;
+                true
+            }
+
+            _ => false,
         }
-        false
     }
 
     fn update(&mut self) {
@@ -189,7 +247,12 @@ impl State {
             // add a pipeline to the renderer
             // this will draw pixel on top of color attachment defined in
             // the RenderPassDescriptor
-            render_pass.set_pipeline(&self.render_pipeline);
+            if self.choose_pipeline {
+                render_pass.set_pipeline(&self.render_pipeline);
+            } else {
+                render_pass.set_pipeline(&self.render_pipeline_color);
+            }
+
             // feed attached pipeline with 3 vertices and 1 instance
             render_pass.draw(0..3, 0..1);
         }
