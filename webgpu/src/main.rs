f209d32ef9d8cdd11bcf752e1f4baba72099e1cc
nicolas cros <nicolas.cros@luos.io>
2021-12-01 13:53:54 +0100

    [webgpu] use a vertex buffer to share data between CPU and GPU

--- webgpu/src/main.rs
+++ webgpu/src/main.rs
@@ -1,9 +1,54 @@
+use wgpu::util::DeviceExt;
 use winit::{
     event::*,
     event_loop::{ControlFlow, EventLoop},
     window::Window,
     window::WindowBuilder,
 };
+
+// main.rs
+#[repr(C)]
+#[derive(Copy, Clone, Debug, bytemuck::Pod, bytemuck::Zeroable)]
+struct Vertex {
+    position: [f32; 3],
+    color: [f32; 3],
+}
+
+impl Vertex {
+    fn desc<'a>() -> wgpu::VertexBufferLayout<'a> {
+        wgpu::VertexBufferLayout {
+            array_stride: std::mem::size_of::<Vertex>() as wgpu::BufferAddress,
+            step_mode: wgpu::VertexStepMode::Vertex,
+            attributes: &[
+                wgpu::VertexAttribute {
+                    offset: 0,
+                    shader_location: 0,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+                wgpu::VertexAttribute {
+                    offset: std::mem::size_of::<[f32; 3]>() as wgpu::BufferAddress,
+                    shader_location: 1,
+                    format: wgpu::VertexFormat::Float32x3,
+                },
+            ],
+        }
+    }
+}
+
+const VERTICES: &[Vertex] = &[
+    Vertex {
+        position: [0.0, 0.5, 0.0],
+        color: [1.0, 0.0, 0.0],
+    },
+    Vertex {
+        position: [-0.5, -0.5, 0.0],
+        color: [0.0, 1.0, 0.0],
+    },
+    Vertex {
+        position: [0.5, -0.5, 0.0],
+        color: [0.0, 0.0, 1.0],
+    },
+];
 
 struct State {
     surface: wgpu::Surface,
@@ -12,8 +57,9 @@ struct State {
     config: wgpu::SurfaceConfiguration,
     size: winit::dpi::PhysicalSize<u32>,
     render_pipeline: wgpu::RenderPipeline,
-    render_pipeline_color: wgpu::RenderPipeline,
     choose_pipeline: bool,
+    vertex_buffer: wgpu::Buffer,
+    num_vertices: u32,
 }
 
 impl State {
@@ -73,7 +119,7 @@ impl State {
             vertex: wgpu::VertexState {
                 module: &shader,
                 entry_point: "vs_main",
-                buffers: &[],
+                buffers: &[Vertex::desc()],
             },
             fragment: Some(wgpu::FragmentState {
                 module: &shader,
@@ -104,45 +150,15 @@ impl State {
             },
         });
 
-        let render_pipeline_color =
-            device.create_render_pipeline(&wgpu::RenderPipelineDescriptor {
-                label: Some("Render Pipeline"),
-                layout: Some(&render_pipeline_layout),
-                vertex: wgpu::VertexState {
-                    module: &shader,
-                    entry_point: "vs_main_color",
-                    buffers: &[],
-                },
-                fragment: Some(wgpu::FragmentState {
-                    module: &shader,
-                    entry_point: "fs_main_color",
-                    targets: &[wgpu::ColorTargetState {
-                        format: config.format,
-                        blend: Some(wgpu::BlendState::REPLACE),
-                        write_mask: wgpu::ColorWrites::ALL,
-                    }],
-                }),
-                primitive: wgpu::PrimitiveState {
-                    topology: wgpu::PrimitiveTopology::TriangleList,
-                    strip_index_format: None,
-                    front_face: wgpu::FrontFace::Ccw,
-                    cull_mode: Some(wgpu::Face::Back),
-                    // Setting this to anything other than Fill requires Features::NON_FILL_POLYGON_MODE
-                    polygon_mode: wgpu::PolygonMode::Fill,
-                    // Requires Features::DEPTH_CLAMPING
-                    clamp_depth: false,
-                    // Requires Features::CONSERVATIVE_RASTERIZATION
-                    conservative: false,
-                },
-                depth_stencil: None,
-                multisample: wgpu::MultisampleState {
-                    count: 1,
-                    mask: !0,
-                    alpha_to_coverage_enabled: false,
-                },
-            });
-
         let choose_pipeline = true;
+
+        let vertex_buffer = device.create_buffer_init(&wgpu::util::BufferInitDescriptor {
+            label: Some("Vertex Buffer"),
+            contents: bytemuck::cast_slice(VERTICES),
+            usage: wgpu::BufferUsages::VERTEX,
+        });
+
+        let num_vertices = VERTICES.len() as u32;
 
         Self {
             surface,
@@ -151,8 +167,9 @@ impl State {
             config,
             size,
             render_pipeline,
-            render_pipeline_color,
             choose_pipeline,
+            vertex_buffer,
+            num_vertices,
         }
     }
 
@@ -247,14 +264,12 @@ impl State {
             // add a pipeline to the renderer
             // this will draw pixel on top of color attachment defined in
             // the RenderPassDescriptor
-            if self.choose_pipeline {
-                render_pass.set_pipeline(&self.render_pipeline);
-            } else {
-                render_pass.set_pipeline(&self.render_pipeline_color);
-            }
+            render_pass.set_pipeline(&self.render_pipeline);
 
+            // set the vertex buffer
+            render_pass.set_vertex_buffer(0, self.vertex_buffer.slice(..));
             // feed attached pipeline with 3 vertices and 1 instance
-            render_pass.draw(0..3, 0..1);
+            render_pass.draw(0..self.num_vertices, 0..1);
         }
         // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
--- webgpu/src/shader.wgsl
+++ webgpu/src/shader.wgsl
@@ -1,34 +1,22 @@
 // Vertex shader
 
-struct VertexOutput {
-    [[builtin(position)]] clip_position: vec4<f32>;
+struct VertexInput {
+    [[location(0)]] position: vec3<f32>;
+    [[location(1)]] color: vec3<f32>;
 };
 
-struct VertexOutputColor {
+struct VertexOutput {
     [[builtin(position)]] clip_position: vec4<f32>;
-    [[location(0)]] position: vec2<f32>;
+    [[location(0)]] color: vec3<f32>;
 };
 
 [[stage(vertex)]]
 fn vs_main(
-    [[builtin(vertex_index)]] in_vertex_index: u32,
+    model: VertexInput,
 ) -> VertexOutput {
     var out: VertexOutput;
-    let x = f32(1 - i32(in_vertex_index)) * 0.5;
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
-    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
-    return out;
-}
-
-[[stage(vertex)]]
-fn vs_main_color(
-    [[builtin(vertex_index)]] in_vertex_index: u32,
-) -> VertexOutputColor {
-    var out: VertexOutputColor;
-    let x = f32(1 - i32(in_vertex_index)) * 0.5;
-    let y = f32(i32(in_vertex_index & 1u) * 2 - 1) * 0.5;
-    out.position = vec2<f32>(x, y);
-    out.clip_position = vec4<f32>(x, y, 0.0, 1.0);
+    out.color = model.color;
+    out.clip_position = vec4<f32>(model.position, 1.0);
     return out;
 }
 
@@ -36,19 +24,7 @@ fn vs_main_color(
 
 [[stage(fragment)]]
 fn fs_main(in: VertexOutput) -> [[location(0)]] vec4<f32> {
-    var out: vec4<f32>;
-    let color_1 = vec4<f32>(0.3, 0.2, 0.1, 1.0);
-    let color_2 = vec4<f32>(0.1, 0.2, 0.5, 1.0);
-    if ((in.clip_position[0] > 400.0) && (in.clip_position[1] > 300.0)) {
-        out = color_1;
-    } else {
-        out = color_2;
-    }
-    return out;
+    return vec4<f32>(in.color, 1.0);
 }
 
-[[stage(fragment)]]
-fn fs_main_color(in: VertexOutputColor) -> [[location(0)]] vec4<f32> {
-    return vec4<f32>(in.position, 0.1, 1.0);
-}
 
