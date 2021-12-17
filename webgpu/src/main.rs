93c39081a46f35da5ab37514e838cccc718eaf09
nicolas cros <nicolas.cros@luos.io>
2021-12-17 13:51:59 +0100

    [webgpu] use a texture from the model file

--- webgpu/src/main.rs
+++ webgpu/src/main.rs
@@ -1,6 +1,7 @@
 mod model;
 mod texture;
 
+use crate::model::DrawModel;
 use cgmath::prelude::*;
 use model::Vertex;
 use wgpu::util::DeviceExt;
@@ -236,7 +237,6 @@ struct State {
     camera_bind_group: wgpu::BindGroup,
     camera_controller: CameraController,
     render_pipeline: wgpu::RenderPipeline,
-    diffuse_bind_group: wgpu::BindGroup,
     instances: Vec<Instance>,
     instance_buffer: wgpu::Buffer,
     depth_texture: texture::Texture,
@@ -282,10 +282,6 @@ impl State {
         };
         surface.configure(&device, &config);
 
-        let diffuse_bytes = include_bytes!("happy-tree.png");
-        let diffuse_texture =
-            texture::Texture::from_bytes(&device, &queue, diffuse_bytes, "happy-tree.png").unwrap();
-
         let texture_bind_group_layout =
             device.create_bind_group_layout(&wgpu::BindGroupLayoutDescriptor {
                 entries: &[
@@ -315,21 +311,6 @@ impl State {
                 ],
                 label: Some("texture_bind_group_layout"),
             });
-
-        let diffuse_bind_group = device.create_bind_group(&wgpu::BindGroupDescriptor {
-            layout: &texture_bind_group_layout,
-            entries: &[
-                wgpu::BindGroupEntry {
-                    binding: 0,
-                    resource: wgpu::BindingResource::TextureView(&diffuse_texture.view),
-                },
-                wgpu::BindGroupEntry {
-                    binding: 1,
-                    resource: wgpu::BindingResource::Sampler(&diffuse_texture.sampler),
-                },
-            ],
-            label: Some("diffuse_bind_group"),
-        });
 
         let shader = device.create_shader_module(&wgpu::ShaderModuleDescriptor {
             label: Some("Shader"),
@@ -489,7 +470,6 @@ impl State {
             camera_bind_group,
             camera_controller,
             render_pipeline,
-            diffuse_bind_group,
             instances,
             instance_buffer,
             depth_texture,
@@ -558,15 +538,14 @@ impl State {
 
             // set the vertex buffer
             render_pass.set_vertex_buffer(1, self.instance_buffer.slice(..));
+            // set the pipeline
             render_pass.set_pipeline(&self.render_pipeline);
-            // use a texture on our mesh
-            render_pass.set_bind_group(0, &self.diffuse_bind_group, &[]);
-            // use our camera transform matrix
-            render_pass.set_bind_group(1, &self.camera_bind_group, &[]);
-
-            use model::DrawModel;
-            render_pass
-                .draw_mesh_instanced(&self.obj_model.meshes[0], 0..self.instances.len() as u32);
+            // render model
+            render_pass.draw_model_instanced(
+                &self.obj_model,
+                0..self.instances.len() as u32,
+                &self.camera_bind_group,
+            );
         }
         // submit will accept anything that implements IntoIter
         self.queue.submit(std::iter::once(encoder.finish()));
