20a34aafc2ca577261873dea169100d0e4f498d4
ncros3 <none>
2022-03-15 22:50:56 +0100

    use typescript and update the circle radius

--- javascript_lab/index.js
+++ javascript_lab/index.js
@@ -1,42 +1,37 @@
 window.onload = function () {
-    let canvas = document.getElementById("target");
-    let ctx = canvas.getContext("2d");
-    let img = document.getElementById("front");
+    var canvas = document.getElementById("target");
+    var ctx = canvas.getContext("2d");
+    var img = document.getElementById("front");
     ctx.drawImage(img, 0, 0);
-}
-
-setInterval(drawCircle, 1000);
-
-let rayon = 100;
-let circle_evolve = true;
-
-function drawCircle() {
-    let canvas = document.getElementById("target");
-    let ctx = canvas.getContext("2d");
-
-    ctx.clearRect(0,0,canvas.clientWidth, canvas.clientHeight);
-
-    if (circle_evolve === true) {
-
-        if (rayon >= 200) {
-            rayon = 200;
-            circle_evolve = false;
-        } else {
-            rayon += 10;
+};
+{
+    var rayon_1 = 100;
+    var circle_evolve_1 = true;
+    function drawCircle() {
+        var canvas = document.getElementById("target");
+        var ctx = canvas.getContext("2d");
+        ctx.clearRect(0, 0, canvas.clientWidth, canvas.clientHeight);
+        if (circle_evolve_1 === true) {
+            if (rayon_1 >= 300) {
+                rayon_1 = 300;
+                circle_evolve_1 = false;
+            }
+            else {
+                rayon_1 += 2;
+            }
         }
-
-    } else {
-
-        if (rayon <= 100) {
-            rayon = 100;
-            circle_evolve = true;
-        } else {
-            rayon -= 10;
+        else {
+            if (rayon_1 <= 100) {
+                rayon_1 = 100;
+                circle_evolve_1 = true;
+            }
+            else {
+                rayon_1 -= 2;
+            }
         }
-
+        ctx.beginPath();
+        ctx.arc(1000, 600, rayon_1, 0, 2 * Math.PI);
+        ctx.stroke();
     }
-
-    ctx.beginPath();
-    ctx.arc(1000, 600, rayon, 0, 2 * Math.PI);
-    ctx.stroke();
+    setInterval(drawCircle, 30);
 }
